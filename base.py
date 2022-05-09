a45cab06ca4c18947a0b3723e372ed9f359104c7
Andrea Dalmasso <55203222+Cocsleboss@users.noreply.github.com>
2022-05-09 18:28:10 +0200

    Update base.py

--- base.py
+++ base.py
@@ -3,14 +3,6 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect("vache.sqlite")  # tante de se connecter a la base de données
-    except sqlite3.error as e:
-        print(e)
-    return conn
-
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
